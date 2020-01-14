211d36ed2881d0a8d48944d83239ac706c5e5848
Kofi Annor <kofiannor@MacBook-2.local>
2020-01-13 20:18:48 -0500

    basic flask app dockerized

--- run.py
+++ run.py
@@ -1,1 +1,12 @@
-print("Hello world!")
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "INTERN API"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
